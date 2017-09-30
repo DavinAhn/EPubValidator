60d9bb148c2effdf685b06277aaf902ed46d00fd
Davin Ahn <m.davinahn@gmail.com>
2017-10-01 01:31:55 +0900

    Remove electron-settings package

--- src/electron/window.js
+++ src/electron/window.js
@@ -1,5 +1,4 @@
 const { BrowserWindow } = require('electron');
-const settings = require('electron-settings');
 const path = require('path');
 const url = require('url');
 
@@ -21,29 +20,27 @@ module.exports = {
   getMain: () => main,
 
   loadMain() {
-    let config = {};
-
-    if (settings.has('mainWindowState')) {
-      config = Object.assign({}, config, settings.get('mainWindowState'));
-    } else {
-      settings.set('mainWindowState', {
-        x: undefined,
-        y: undefined,
-        width: 600,
-        minWidth: 600,
-        height: 800,
-        minHeight: 800,
-        fullscreenable: false,
-      });
-    }
-
     const pathname = path.join(__dirname, '../../index.html');
-    const win = this.create(pathname, config);
+    const win = this.create(pathname, {
+      x: undefined,
+      y: undefined,
+      width: 600,
+      minWidth: 600,
+      height: 800,
+      minHeight: 800,
+      fullscreenable: false,
+      show: false,
+      frame: false,
+      backgroundColor: '#1e2028',
+    });
     win.on('closed', () => {
       // Dereference the window object, usually you would store windows
       // in an array if your app supports multi windows, this is the time
       // when you should delete the corresponding element.
       main = null;
+    });
+    win.once('ready-to-show', () => {
+      win.show();
     });
 
     main = win;
