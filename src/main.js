7da541cf4399954642e31cd891a4aafe493d9898
Davin Ahn <m.davinahn@gmail.com>
2017-09-30 16:47:45 +0900

    Add .eslintrc.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron');
+const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
 
@@ -8,13 +8,13 @@ let win;
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600});
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
-    slashes: true
+    slashes: true,
   }));
 
   // Open the DevTools.
@@ -22,10 +22,10 @@ function createWindow() {
 
   // Emitted when the window is closed.
   win.on('closed', () => {
-	// Dereference the window object, usually you would store windows
+    // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    win = null;
   });
 }
 
