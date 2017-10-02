bf20ec4be73580d36be18eb02718438c4fac2c66
Davin Ahn <m.davinahn@gmail.com>
2017-10-03 01:57:17 +0900

    Grouped results by validator

--- src/electron/verfiy.js
+++ src/electron/verfiy.js
@@ -2,13 +2,21 @@ const { EPubValidator } = require('./validator/EPubValidator');
 
 module.exports = {
   run(epubPath, mimeType, callback) {
+    const results = {
+      append: function(name, result, error) {
+        this.orders.push(name);
+        this[name] = { result, error };
+      },
+      orders: [],
+    };
     new Promise((resolve, reject) => {
       const ev = new EPubValidator(epubPath);
       ev.run((result, error) => {
-        resolve(result, error);
+        results.append(ev.name, result, error);
+        resolve(results);
       });
-    }).then((result, error) => {
-      callback(result, error);
+    }).then((results) => {
+      callback(results);
     });
   }
 };
